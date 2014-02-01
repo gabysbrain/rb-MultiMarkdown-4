604b732a1ac40b0d77d9ca2cfc361dd63e950822
Thomas Torsney-Weir <torsneyt@gmail.com>
2014-02-01 18:46:19 +0100

    change directories once per test

--- test/test_writer.rb
+++ test/test_writer.rb
@@ -10,10 +10,10 @@ def build_tests(subdir, exclude_exts: [], ext_map: {}, options: [])
   # merge the extension map
   ext_map = EXT_MAP.merge(ext_map)
 
-  Dir.chdir(File.join(BASE_TEST_DIR, subdir))
-  Dir.glob("*.text").each do |source|
+  test_dir = File.join(BASE_TEST_DIR, subdir)
+  Dir.glob(File.join(test_dir, "*.text")).each do |source|
     test_rawname = File.basename(source, ".text")
-    test_targets = Dir.glob(test_rawname+".*")
+    test_targets = Dir.glob(File.join(test_dir, test_rawname+".*"))
     test_targets.select! {|fname| not fname.end_with?(".text")}
     target_formats = test_targets.map {|fn| File.extname(fn)[1..-1]}
     exclude_exts.each do |ext|
@@ -33,10 +33,26 @@ def build_tests(subdir, exclude_exts: [], ext_map: {}, options: [])
 
       # test code is here
       define_method(test_name) do 
+        orig_dir = Dir.pwd
+        Dir.chdir(File.join(BASE_TEST_DIR, subdir))
         source_txt = File.read(source)
         target_txt = File.read(target_file)
         parser = MultiMarkdown::Parser.new(source_txt, *options)
-        assert_equal(target_txt, parser.convert(target))
+
+        parsed_src = parser.convert(target)
+
+        # clean up both outputs
+        target_txt.gsub!(/\s+\z/, "")
+        parsed_src.gsub!(/\s+\z/, "")
+        if use_tidy
+          target_txt.gsub!("'", "\\\\'")
+          parsed_src.gsub!("'", "\\\\'")
+          target_txt = `echo "#{target_txt}" | tidy --show-body-only 1 --quiet 1 --show-warnings 0`
+          parsed_src = `echo "#{parsed_src}" | tidy --show-body-only 1 --quiet 1 --show-warnings 0`
+        end
+
+        Dir.chdir(orig_dir)
+        assert_equal(target_txt, parsed_src)
       end
     end
   end
