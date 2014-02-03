8ee74a2be8e6edd6259ca8cf3438b3abe3ec8221
Thomas Torsney-Weir <torsneyt@gmail.com>
2014-02-03 13:38:31 +0100

    move the mmd command check into the module

--- lib/multimarkdown-cli.rb
+++ lib/multimarkdown-cli.rb
@@ -1,23 +1,28 @@
 require "multimarkdown-cli/version"
 
-# check that the command line tool is installed when requiring
-# from http://stackoverflow.com/questions/2108727/which-in-ruby-checking-if-program-exists-in-path-from-ruby
-exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
-mmd_cmd_found = false
-ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-  exts.each do |ext|
-    exe = File.join(path, "multimarkdown#{ext}")
-    if File.executable? exe
-      mmd_cmd_found = true
-      break
-    end
-  end
-end
-unless mmd_cmd_found
-  raise LoadError.new("Can't find the multimarkdown command in your path.  Please install it")
-end
-
 module MultiMarkdownCLI
+  def self._cli_installed?
+    # from http://stackoverflow.com/questions/2108727/which-in-ruby-checking-if-program-exists-in-path-from-ruby
+    exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+    mmd_cmd_found = false
+    ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+      exts.each do |ext|
+        exe = File.join(path, "multimarkdown#{ext}")
+        if File.executable? exe
+          mmd_cmd_found = true
+          break
+        end
+      end
+    end
+    mmd_cmd_found
+  end
+
+  # check that the command line tool is installed when requiring
+  unless _cli_installed?
+    raise LoadError.new("Can't find the multimarkdown command in your path.  Please install it")
+  end
+
+
   class Parser
     attr_reader :source
 
